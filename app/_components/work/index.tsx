72a83236b33b745e173649ec40b3ab239bc58e3d
Sasha <w24005@osfl.ac.jp>
2026-01-29 11:50:55 +0900

    new project

--- app/_components/work/index.tsx
+++ app/_components/work/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styles from "./index.module.css";
+import Image from "next/image";
 
 export default function WorkPage() {
   return (
@@ -20,30 +21,79 @@ export default function WorkPage() {
       <p className={styles.text}>どうぞごゆっくりご覧ください。</p>
       <ul className={styles.workList}>
         <li className={styles.workItem}>
-          <h3>企業サイト制作</h3>
+          <h3>Sweet Cake Bakeshop | 洋菓子店のホームページ制作</h3>
           <br />
-          <p>TypeScriptを使用したポートフォリオサイト</p>
+          <p>HTMLを使用したシンプルなウェブサイト</p>
           <Link
-            href="https://github.com/pesocheck1/project1"
+            href="https://cakeshop-lemon.vercel.app/"
             target="_blank"
             className={styles.link}
           >
-            GitHub リポジトリはこちら
+            サイトはこちら
           </Link>
-        </li>
-        <li className={styles.workItem}>
-          <h3>洋菓子店のホームページ制作</h3>
           <br />
-          <p>HTMLを使用したシンプルなウェブサイト</p>
           <Link
             href="https://github.com/pesocheck1/cakeshop"
             target="_blank"
             className={styles.link}
           >
-            GitHub リポジトリはこちら
+            GitHubリポジトリはこちら
           </Link>
         </li>
-        {/* 必要に応じてリストを追加 */}
+        <li className={styles.workItem}>
+          <h3>Sweet Cupcake Shop | お菓子のオンラインショップ制作</h3>
+          <br />
+          <p>
+            TypeScriptを使用した商品カタログと注文機能を備えているお菓子販売サイト
+          </p>
+          <Link
+            href="https://orderapp-eight.vercel.app/"
+            target="_blank"
+            className={styles.link}
+          >
+            サイトはこちら
+          </Link>
+          <br />
+          <Link
+            href="https://github.com/pesocheck1/orderapp"
+            target="_blank"
+            className={styles.link}
+          >
+            GitHubリポジトリはこちら
+          </Link>
+        </li>
+        <li className={styles.workItemlast}>
+          <h3>TravelQuest Okinawa | 旅行ルート作成Webアプリ</h3>
+          <br />
+          <p>沖縄旅行のルートを直感的に楽しく作成できるWebアプリ。</p>
+          <p>
+            TypeScriptを使用し、地図上でのルート表示や移動距離・時間の計算などを実装。
+          </p>
+          <p> チームではフロントエンドと画面設計を担当しました。</p>
+          <Link
+            href="https://travelquestokinawa.vercel.app/"
+            target="_blank"
+            className={styles.link}
+          >
+            サイトはこちら
+          </Link>
+          <br />
+          <Link
+            href="https://github.com/pesocheck1/travelquest"
+            target="_blank"
+            className={styles.link}
+          >
+            GitHubリポジトリはこちら
+          </Link>
+          <br />
+          <Image
+            src="/images/screenshot.png"
+            alt="travelquest"
+            width={700}
+            height={500}
+            className={styles.screenshot}
+          />
+        </li>
       </ul>
     </main>
   );
